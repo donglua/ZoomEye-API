bd8163a90b72e9294b066390bdc8bb06286072cd
黄东鲁 <foolfalcon@hotmail.com>
2016-04-29 22:30:36 +0800

    修改 Match 对象

--- zoomeye-api/src/main/java/org/zoomeye/api/search/Match.java
+++ zoomeye-api/src/main/java/org/zoomeye/api/search/Match.java
@@ -10,5 +10,19 @@ public class Match {
   private PortInfo portinfo;
   private String timestamp; // 2016-04-19T16:34:28
 
+  public GeoInfo getGeoinfo() {
+    return geoinfo;
+  }
 
+  public String getIp() {
+    return ip;
+  }
+
+  public PortInfo getPortinfo() {
+    return portinfo;
+  }
+
+  public String getTimestamp() {
+    return timestamp;
+  }
 }
